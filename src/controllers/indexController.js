7a9a182eb525c6c92da660c6ffff2c61a2181548
LeandroBoxler <leandroboxler@gmail.com>
2023-12-09 00:05:03 -0300

    index cambio en productos

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,13 +1,18 @@
 const  {JSON} = require ("../data")
 const products = JSON('products')
+const cupones = JSON('cupones')
+const catalogo = ["aventura","accion"]
 
+const categorias = function(parametro){ 
+    
+    return pepe = products.filter(a=> a.categorias == parametro)
+}
 
 module.exports = {
     index : (req,res) => {
-        
-        return res.render('index',{products})
-    
-    
+
+        return res.render('index',{products,categorias,catalogo})
+
     },
 
 
@@ -22,9 +27,9 @@ module.exports = {
     /*----CUPONES--------*/
     
     const {cupon} = req.body
-    const JSONcupones = JSON('cupones')
+   
 
-    j = JSONcupones.find(a => a.id == cupon) || {descuento:0}
+    j = cupones.find(a => a.id == cupon) || {descuento:0}
     
     if (j.descuento) {
        const totalis = total / 100 * j.descuento
@@ -32,10 +37,6 @@ module.exports = {
     }
     else{ const totalis = total
         return res.render('productcart',{products,totalis})}
-
-    
-    
-    
     },
 
 }
